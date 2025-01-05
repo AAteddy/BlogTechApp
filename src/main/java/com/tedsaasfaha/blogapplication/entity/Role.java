0597db32ef2651c9cc1e3c6e3e377ab15efba1d7
Tedros Alem <tedsaasfaha@gmail.com>
2025-01-05 19:01:44 +0300

    implement user entity class and userregistration and userlogin dtos

---
+++ src/main/java/com/tedsaasfaha/blogapplication/entity/Role.java
@@ -0,0 +1,6 @@
+package com.tedsaasfaha.blogapplication.entity;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/tedsaasfaha/blogapplication/entity/User.java
@@ -0,0 +1,35 @@
+package com.tedsaasfaha.blogapplication.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
