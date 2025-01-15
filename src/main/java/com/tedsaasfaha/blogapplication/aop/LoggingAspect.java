49cd9067500e34bf2d9e0baae3fb990377fdc0ef
Tedros Alem <tedsaasfaha@gmail.com>
2025-01-15 23:33:43 +0300

    implement logger info and warning on key methods of PostServiceImpl class

--- src/main/java/com/tedsaasfaha/blogapplication/aop/LoggingAspect.java
+++ src/main/java/com/tedsaasfaha/blogapplication/aop/LoggingAspect.java
@@ -1,3 +1,4 @@
+
 package com.tedsaasfaha.blogapplication.aop;
 
 
@@ -7,6 +8,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 
 @Aspect
 @Component
@@ -14,21 +17,28 @@ public class LoggingAspect {
 
     private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
-    @Pointcut("within(com.tedsaasfaha.blogapplication.service..*)")
-    public void serviceMethods() {}
+    @Pointcut("execution(* com.tedsaasfaha.blogapplication.service.PostServiceImpl.*(..))")
+    public void postServiceMethods() {}
 
-    @Before("serviceMethods()")
+    @Before("postServiceMethods()")
     public void logBefore(JoinPoint joinPoint) {
-        logger.info("Entering method: {} with arguments: {}", joinPoint.getSignature(),joinPoint.getArgs());
+        logger.info("Executing method: {} with arguments: {}",
+                joinPoint.getSignature().getName(),
+                Arrays.toString(joinPoint.getArgs()));
     }
 
-    @AfterReturning(pointcut = "serviceMethods()", returning = "result")
+    @AfterReturning(pointcut = "postServiceMethods()", returning = "result")
     public void logAfterReturning(JoinPoint joinPoint, Object result) {
-        logger.info("Exiting method: {} with result: {}", joinPoint.getSignature(), result);
+        logger.info("Exiting method: {} with result: {}",
+                joinPoint.getSignature().getName(),
+                result);
     }
 
-    @AfterThrowing(pointcut = "serviceMethods()", throwing = "exception")
+    @AfterThrowing(pointcut = "postServiceMethods()", throwing = "exception")
     public void logAfterThrowing(JoinPoint joinPoint, Throwable exception) {
-        logger.error("Exception in method: {} with message: {}", joinPoint.getSignature(), exception.getMessage());
+        logger.error("Exception in method: {} with message: {}",
+                joinPoint.getSignature().getName(),
+                exception.getMessage());
     }
 }
+//
