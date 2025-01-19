95b81909a68746dd723e10e0c55a5ce258f95341
Tedros Alem <tedsaasfaha@gmail.com>
2025-01-19 13:57:23 +0300

    implement AdminRegistrationDTO for admin registration request data

---
+++ src/main/java/com/tedsaasfaha/blogapplication/dto/AdminRegistrationDTO.java
@@ -0,0 +1,22 @@
+package com.tedsaasfaha.blogapplication.dto;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class AdminRegistrationDTO {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @Email(message = "Invalid Email Address")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
+    private String password;
+}
